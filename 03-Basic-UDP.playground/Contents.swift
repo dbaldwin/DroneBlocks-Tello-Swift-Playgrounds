53cdbd5512185e771ad581bf5b28d9aa48e1d828
Dennis Baldwin <dennisbaldwin@gmail.com>
2019-07-17 15:50:52 -0500

    Updating project to get SwiftSocket working

--- 03-Basic-UDP.playground/Contents.swift
+++ 03-Basic-UDP.playground/Contents.swift
@@ -2,3 +2,5 @@ import UIKit
 import SwiftSocket
 
 let client: UDPClient = UDPClient(address: "127.0.0.1", port: 8889)
+
+
--- Pods/Target Support Files/Pods-DroneBlocks-Tello-Swift-Playground/Pods-DroneBlocks-Tello-Swift-Playground-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DroneBlocks_Tello_Swift_Playground : NSObject
-@end
-@implementation PodsDummy_Pods_DroneBlocks_Tello_Swift_Playground
-@end
