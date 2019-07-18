441cd29e312e72d01a498fec36d18cd10211b760
Dennis Baldwin <dennisbaldwin@gmail.com>
2019-07-18 10:02:47 -0500

    Testing socket send

--- 03-Basic-UDP.playground/Contents.swift
+++ 03-Basic-UDP.playground/Contents.swift
@@ -2,5 +2,6 @@ import UIKit
 import SwiftSocket
 
 let client: UDPClient = UDPClient(address: "127.0.0.1", port: 8889)
+let message = "command"
 
-
+client.send(data: message.data(using: String.Encoding.utf8)!)
