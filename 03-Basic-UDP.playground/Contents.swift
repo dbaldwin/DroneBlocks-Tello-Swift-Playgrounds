9af111e4dea6e18d6dd7b29e8d5760f2b1470287
Dennis Baldwin <dennisbaldwin@gmail.com>
2019-08-02 19:56:02 -0500

    Experimenting with Tello EDU swarm

--- 03-Basic-UDP.playground/Contents.swift
+++ 03-Basic-UDP.playground/Contents.swift
@@ -1,7 +1,16 @@
 import UIKit
 import SwiftSocket
 
-let client: UDPClient = UDPClient(address: "127.0.0.1", port: 8889)
+let drone1: UDPClient = UDPClient(address: "127.0.0.1", port: 8889)
+let drone2: UDPClient = UDPClient(address: "127.0.0.1", port: 9000)
+
 let message = "command"
 
-client.send(data: message.data(using: String.Encoding.utf8)!)
+drone1.send(data: message.data(using: String.Encoding.utf8)!)
+
+struct drones {
+    let drone1: String
+    let drone2: String
+}
+
+print("done")
